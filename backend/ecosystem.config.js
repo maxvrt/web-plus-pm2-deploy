f10644ee6e8f3f7ed8cc25eff9d5a1fafe935e4d
Maksim Sokolov <vonrut@gmail.com>
2023-03-28 17:31:22 +0300

    frnt deploy

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+  env: {
+    NODE_ENV: 'production',
+  },
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': 'scp -Cr ./.env vortm1@158.160.24.251:/home/vortm1/mesto/backend',
+      'post-deploy': 'cd ~/mesto/source/backend/ && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
