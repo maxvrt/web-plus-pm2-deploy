c3f8f145c7e2ba78e816a4ce0abbf571bdecc3d0
Maksim Sokolov <vonrut@gmail.com>
2023-03-28 19:54:47 +0300

    bcknd deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -21,8 +21,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      // 'pre-deploy': 'cd ~/m-backend/source/backend',
-      'post-deploy': 'cd ~/m-backend/source/backend/ && npm i && npm run build && cd ~/m-backend/source/backend/dist/ && pm2 start app.js',
+      'post-deploy': 'cd ~/m-backend/source/backend/ && npm i && npm run build && cd ~/m-backend/source/backend/dist/ && pm2 startOrRestart app.js',
     },
   },
 };
