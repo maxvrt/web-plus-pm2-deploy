809066300d18380cf49da178b6fcb4706a1ab75f
Maksim Sokolov <vonrut@gmail.com>
2023-03-29 20:02:34 +0300

    правки

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -21,7 +21,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd ~/m-backend/source/backend/ && npm i && npm run build && && pm2 restart ecosystem.config.js && pm2 save',
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': 'cd ~/m-backend/source/backend/ && npm i && npm run build && pm2 restart ecosystem.config.js && pm2 save',
     },
   },
 };
